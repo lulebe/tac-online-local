de2b9c64a2e21956990129b6ce1ddf0db2dc1437
Leander Berg <reg@lulebe.net>
2020-11-16 21:25:04 +0100

    fixed screens

--- static/js/screen/game.js
+++ static/js/screen/game.js
@@ -1,18 +1,11 @@
-const socket = io(window.location.origin);
+const STONE_POSITION_BOX = 0
+const STONE_POSITION_FIELD = 1
+const STONE_POSITION_HOUSE = 2
+const TEAMMATE_INDEX = [2,3,0,1]
+
+const socket = io(window.location.origin)
 socket.on('connect', () => {
   socket.emit('screen-name', gameName)
-})
-socket.on('players', function(data) {
-  if (!game) return
-  const connected = {}
-  data.forEach(d => {
-    connected[d.name] = d.connected
-  })
-  game.players.forEach(player => {
-    player.connected = connected[player.name]
-  })
-  updateScoreboard()
-  updateDeckdata()
 })
 socket.on('game-data', data => {
   console.log(data)
@@ -26,16 +19,12 @@ socket.on('warning', data => {
   displayWarning(data.warningNum)
 })
 
-const STONE_POSITION_BOX = 0
-const STONE_POSITION_FIELD = 1
-const STONE_POSITION_HOUSE = 2
-const TEAMMATE_INDEX = [2,3,0,1]
+const SE_CLICKED_FIELD = 1
 
 let game = null
 let turnData = null
 initCanvas()
 
-const SE_CLICKED_FIELD = 1
 function boardClicked (data) {
   socket.emit('screen-event', {gameName, event: SE_CLICKED_FIELD, data})
 }
